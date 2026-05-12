248fbfd4a9c52821e57704c19da11074cccabc94
Frankemeka92 <emekafrankline92@gmail.com>
2026-05-12 16:11:25 +0300

    responsiveness added

---
+++ attendance/js/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyB1eCBz0MU5nV8pmSVeWmddXo7WPmbuwTQ",
+  authDomain: "attendance-91a5e.firebaseapp.com",
+  databaseURL: "https://attendance-91a5e-default-rtdb.firebaseio.com",
+  projectId: "attendance-91a5e",
+  storageBucket: "attendance-91a5e.firebasestorage.app",
+  messagingSenderId: "192391742455",
+  appId: "1:192391742455:web:0d7f773f3e4e3f65e8e4f2",
+  measurementId: "G-X1J35F427Q"
+};
